131c49dd9a4b0acb511bd726b6c409634c76837c
Mike Meadows <mmeadow33@gmail.com>
2016-06-17 13:25:39 -0500

    all buttons now work and log out math

--- calculator.js
+++ calculator.js
@@ -4,24 +4,18 @@
 // Copy the code below an implement a basic calculator.
 // When the user performs one of the operations, output the result to another DOM element of your choice.
 
-var x = document.getElementById("firstNum").value;
-var y = document.getElementById("secondNum").value;
-var multiplyButton = document.getElementById("multiply").addEventListener("click", doMath());
+
+
 var multiply = function(x, y) {
 	return (x * y);
 }
-
-var addButton = document.getElementById("addBtn").addEventListener("click", doMath);
 var add = function(x, y) {
 	return(x + y);
 }
-
-var subtractButton = document.getElementById("subtract").addEventListener("click", doMath);
 var subtract = function(x, y) {
 	return (x - y);
 }
 
-var divideButton = document.getElementById("divide").addEventListener("click", doMath);
 var divide = function(x, y) {
 	return (x / y);
 }
@@ -30,5 +24,33 @@ function doMath(x, y, func) {
   return func(x,y);
 };
 console.log(doMath(3,5,add))
-
-
+console.log(doMath(3,5,multiply))
+var answer = document.getElementById("output")
+////////calls multiply function///////////////////////
+var multiplyButton = document.getElementById("multiply").addEventListener("click", function (event) {
+  var x = parseInt(document.getElementById("firstNum").value);
+  var y = parseInt(document.getElementById("secondNum").value);
+  var result = parseInt(doMath(x, y, multiply));
+  console.log(result)
+});
+/////////calls Addition function////////////////////////////////
+var addButton = document.getElementById("addBtn").addEventListener("click", function (event) {
+  var x = parseInt(document.getElementById("firstNum").value);
+  var y = parseInt(document.getElementById("secondNum").value);
+  var result = parseInt(doMath(x, y, add));
+  console.log(result)
+});
+///////////////////calls Addition function////////////////////////////////
+var subButton = document.getElementById("subtract").addEventListener("click", function (event) {
+  var x = parseInt(document.getElementById("firstNum").value);
+  var y = parseInt(document.getElementById("secondNum").value);
+  var result = parseInt(doMath(x, y, subtract));
+  console.log(result)
+});
+///////////////////calls Addition function////////////////////////////////
+var divideButton = document.getElementById("divide").addEventListener("click", function (event) {
+  var x = parseInt(document.getElementById("firstNum").value);
+  var y = parseInt(document.getElementById("secondNum").value);
+  var result = parseInt(doMath(x, y, divide));
+  console.log(result)
+});
